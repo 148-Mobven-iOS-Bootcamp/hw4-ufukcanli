9e4a325a43ed8d97fff1234ee11814c543ad41b0
ufukcanli <iletisim@ufukcanli.com>
2022-01-12 14:53:30 +0300

    Add map view and check status

---
+++ WebKitApp/WebKitApp/WebViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebViewController.swift
+//  WebKitApp
+//
+//  Created by Ufuk Canlı on 11.01.2022.
+//
+
+import UIKit
+import WebKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    
+}
